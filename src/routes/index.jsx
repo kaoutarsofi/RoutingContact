a84db5a818ac4e8cca601156f2a0520bdc7ab412
kaoutar asus <kaoutarbattah@gmail.com>
2024-12-27 18:36:03 +0100

    first commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,9 @@
+import { MdContactMail } from "react-icons/md";
+import { RiContactsBook3Line } from "react-icons/ri";
+
+export default function Index() {
+  return (
+    <div id="szero-state">
+      </div>
+  );
+}
